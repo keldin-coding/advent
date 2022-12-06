a7951f7ca6117c623890c0b30e834c678bd4db6c
Jonathon Anderson <coding@jpanderson.dev>
2022-12-06 00:20:09 -0800

    Mix task for running

--- 2022/solution_ex/mix.exs
+++ 2022/solution_ex/mix.exs
@@ -7,7 +7,8 @@ defmodule SolutionEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -24,5 +25,18 @@ defmodule SolutionEx.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp aliases() do
+    [
+      problem: &invoke_problem/1
+    ]
+  end
+
+  defp invoke_problem([problem_number]) do
+    # IO.puts("and we got #{problem_number}")
+    padded_num = problem_number |> String.pad_leading(2, "0")
+
+    Mix.Task.run("run", ["./lib/runners/day#{padded_num}.exs"])
   end
 end
